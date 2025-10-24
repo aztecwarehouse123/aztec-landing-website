96b78cc66ee4ebcc246968566eadf89f15114581
umer2k200 <umarzeeshan709@gmail.com>
2025-10-24 16:09:12 +0500

    deploying again

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import {BrowserRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router basename='/aztec-landing-website/'>
-      <App />
-    </Router>
+    <App />
   </StrictMode>
 );
